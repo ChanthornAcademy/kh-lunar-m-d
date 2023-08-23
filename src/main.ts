6c1d5f3ca35320e96cc0664703fbd0d1d6a781b3
chanthornAcademy <hoeurnchanthorn@gmail.com>
2023-08-23 11:20:41 +0700

    fixed bugs

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,33 @@
 import momentkh from "@thyrith/momentkh";
 import moment from "moment";
-
 import plugin from "../types";
-
 import { PluginFunc } from "dayjs";
+
+let numberMap = {
+  "១": 1,
+  "២": 2,
+  "៣": 3,
+  "៤": 4,
+  "៥": 5,
+  "៦": 6,
+  "៧": 7,
+  "៨": 8,
+  "៩": 9,
+  "០": 0,
+};
+
+const toNumber = (number: string) => {
+  return number.toString().replace(/[០១២៣៤៥៦៧៨៩]/g, (m) => numberMap[m]);
+};
 
 const toKhDate: PluginFunc<plugin.toKhDate> = (o, c, d) => {
   const proto = c.prototype;
   const date = momentkh(moment);
   proto.toKhDate = function (format?: string) {
-    return date(this.format()).toKhDate(format);
+    return date(toNumber(this.format())).toKhDate(format);
   };
   proto.khNewYear = function () {
-    return date.getKhNewYearMoment(this.year());
+    return date.getKhNewYearMoment(toNumber(this.year()));
   };
 };
 
